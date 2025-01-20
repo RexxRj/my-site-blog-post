55fbbdf55f298e9b539b665186dfb4dc0fae55a7
RexxRj <103634570+RexxRj@users.noreply.github.com>
2025-01-21 01:52:15 +0530

    updated DEBUG setting

--- my_site/settings.py
+++ my_site/settings.py
@@ -130,18 +130,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
+STATIC_URL = '/static/'
 
-print("DEBUG:", DEBUG is False)
-print("DEBUG TYPE", type(DEBUG))
-if DEBUG is False:
+if DEBUG == 'False':
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    print(STATICFILES_STORAGE)
+    
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
